cade4551ee658b03aa1185ccb0ededaa8be6d6d1
victor huanchi <victorhuanchi@outlook.es>
2016-04-13 10:13:00 -0500

    tercer archivo: ejercicio03

---
+++ ejercicio03.rb
@@ -0,0 +1,12 @@
+videos=["BUSQUEDA IMPLACABLE","MISION IMPOSIBLE","CORRER O MORIR","MADA MAX (FURIA EN EL CAMINO)","DIVERGENTES"]
+    puts "MI PELICULA FAVORITO ES: " + videos[0]
+    puts "MI PELICULA FAVORITO ES: " + videos[1]
+    puts "MI PELICULA FAVORITO ES: " + videos[2]
+    puts "MI PELICULA FAVORITO ES: " + videos[3]
+    puts "MI PELICULA FAVORITO ES: " + videos[4]
+    puts "******************************************************"
+    puts "******************************************************"
+     videos.each do  |indice|  
+    puts "MI PELICULA FAVORITO ES: " + indice
+    end
+    gets()    
